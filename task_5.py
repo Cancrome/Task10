e6a6de0bf10c7f418dbfc115dda364e1c146eb5d
Mary <marysok-16@mail.ru>
2023-03-27 18:18:44 +0600

    Solution lesson 5

--- task_5.py
+++ task_5.py
@@ -7,3 +7,19 @@
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
 """
+
+import subprocess
+
+ping= ''
+
+links = ['ping', 'yandex.ru']
+subproc_ping = subprocess.Popen(links, stdout=subprocess.PIPE)
+for line in subproc_ping.stdout:
+    ping += line.decode('cp866')
+
+links2 = ['ping', 'youtube.com']
+subproc_ping = subprocess.Popen(links2, stdout=subprocess.PIPE)
+for line in subproc_ping.stdout:
+    ping += line.decode('cp866')
+
+print(ping.encode('utf-8').decode('utf-8'))
